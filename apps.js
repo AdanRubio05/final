adec33255dedbc25086bc8fc7bf8d5431de447dc
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-11-14 13:42:43 -0600

    javascript added and fades added

---
+++ apps.js
@@ -0,0 +1,17 @@
+const game = ()=>{
+    const startGame = ()=>{
+        const playBtn = document.querySelector('.intro button') ;
+        const introScreen = document.querySelector('.intro') ;
+        const puzzle = document.querySelector('.puzzle') ;
+
+        playBtn.addEventListener('click', ()=>{
+            introScreen.classList.add('fadeOut') ;
+        });
+    };
+
+    //inner function call
+    startGame();
+};
+
+//game call
+game();
